45e7eec7c76caf831ef06f97996592353a6a4e26
Jéssica Cruz <jessica.santos648@gmail.com>
2020-06-09 21:10:38 -0300

    Rotas

--- frontend/src/app/photos/photos.module.ts
+++ frontend/src/app/photos/photos.module.ts
@@ -3,12 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent
     ],
-    imports: [ HttpClientModule ]
+    imports: [ HttpClientModule, CommonModule ]
 })
 export class PhotosModule { }
